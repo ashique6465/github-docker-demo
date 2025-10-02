7075d371543e7fb7b61d524bcf50179bd88c357b
ashique6465 <mdashiqueali71@gmail.com>
2025-10-02 18:53:45 +0530

    Test

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-console.assert( 1 + 1 !== 2, "Math is broken");
-console.log("All tests passed!");
+console.log("Running tests...");
+
+if (1 + 1 !== 2) {
+  console.error("Test failed!");
+  process.exit(1); // EXIT WITH ERROR → workflow fails
+} else {
+  console.log("All tests passed ✅");
+}
