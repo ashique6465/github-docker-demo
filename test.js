03a8f396886c0206d9a63fd2bf0127be4c259ecf
ashique6465 <mdashiqueali71@gmail.com>
2025-10-02 19:16:09 +0530

    jest test case

--- test.js
+++
@@ -1,8 +0,0 @@
-console.log("Running tests...");
-
-if (1 + 1 !== 3) {
-  console.error("Test failed!");
-  process.exit(1); // EXIT WITH ERROR → workflow fails
-} else {
-  console.log("All tests passed ✅");
-}
